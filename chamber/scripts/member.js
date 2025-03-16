e081db1c868721868d71672d58e7254ffe9317c1
adlermo <adlerorteney@gmail.com>
2025-03-16 00:11:26 -0300

    final touches

--- chamber/scripts/member.js
+++ chamber/scripts/member.js
@@ -8,7 +8,7 @@ const displayMembers = (members) => {
         const memberCard = document.createElement("section");
         memberCard.classList.add("member");
         memberCard.innerHTML = `
-            <img src="../images/${member.logo}" alt="${member.name}">
+            <img src="images/${member.logo}" alt="${member.name}">
             <h3>${member.name}</h3>
             <p>${member.address}</p>
             <p>${member.phone}</p>
