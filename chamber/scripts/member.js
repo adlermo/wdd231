9c15ac8e9e5bad506c97ef67a5184cd883e5dce4
adlermo <adlerorteney@gmail.com>
2025-03-15 23:38:22 -0300

    adding random image

---
+++ chamber/scripts/member.js
@@ -0,0 +1,47 @@
+// Description: This file contains the code to display the members of the chamber in a grid or list view.
+
+// Display members function
+const displayMembers = (members) => {
+    const display = document.querySelector("article");
+    display.innerHTML = "";
+    members.forEach(member => {
+        const memberCard = document.createElement("section");
+        memberCard.classList.add("member");
+        memberCard.innerHTML = `
+            <img src="../images/${member.logo}" alt="${member.name}">
+            <h2>${member.name}</h2>
+            <p>${member.address}</p>
+            <p>${member.phone}</p>
+            <p>${member.website}</p>
+        `;
+        display.appendChild(memberCard);
+    });
+};
+
+// Request to members of the chamber in ../data/members.json
+fetch("https://adlermo.github.io/wdd231/chamber/data/members.json").then(response => {
+    return response.json();
+}).then(data => {
+    displayMembers(data);
+}).catch(error => {
+    console.log(error);
+});
+
+// Toggle between grid and list view function
+
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+
+const display = document.querySelector("article");
+
+gridbutton.addEventListener("click", () => {
+    display.classList.add("grid");
+    display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", showList);
+
+function showList() {
+    display.classList.add("list");
+    display.classList.remove("grid");
+}
