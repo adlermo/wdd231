101938845212736c683737ddff232d641647027c
adlermo <adlerorteney@gmail.com>
2025-04-12 00:06:57 -0300

    Rename searchParams

--- chamber/scripts/join-form.js
+++ chamber/scripts/join-form.js
@@ -1,30 +1,30 @@
 // Retrieving data from the URL parameters
-const urlParams = new URLSearchParams(window.location.search);
+const searchParams = new URLSearchParams(window.location.search);
 
 // and displaying it in the HTML elements
 document.getElementById('display-first-name')
-    .textContent = urlParams.get('first-name') || 'N/A';
+    .textContent = searchParams.get('first-name') || 'N/A';
 
 document.getElementById('display-last-name')
-    .textContent = urlParams.get('last-name') || 'N/A';
+    .textContent = searchParams.get('last-name') || 'N/A';
 
 document.getElementById('display-organization-title')
-    .textContent = urlParams.get('organization-title') || 'N/A';
+    .textContent = searchParams.get('organization-title') || 'N/A';
 
 document.getElementById('display-email')
-    .textContent = urlParams.get('email') || 'N/A';
+    .textContent = searchParams.get('email') || 'N/A';
 
 document.getElementById('display-phone')
-    .textContent = urlParams.get('phone') || 'N/A';
+    .textContent = searchParams.get('phone') || 'N/A';
 
 document.getElementById('display-company')
-    .textContent = urlParams.get('company') || 'N/A';
+    .textContent = searchParams.get('company') || 'N/A';
 
 document.getElementById('display-membership-type')
-    .textContent = urlParams.get('membership-type') || 'N/A';
+    .textContent = searchParams.get('membership-type') || 'N/A';
 
 document.getElementById('display-description')
-    .textContent = urlParams.get('description') || 'N/A';
+    .textContent = searchParams.get('description') || 'N/A';
 
 document.getElementById('display-timestamp')
-    .textContent = urlParams.get('timestamp') || 'N/A';
+    .textContent = searchParams.get('timestamp') || 'N/A';
